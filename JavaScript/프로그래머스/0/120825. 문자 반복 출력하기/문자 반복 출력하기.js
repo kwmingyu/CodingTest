fed11b6c1c55f84b0c654a7b8d8ddf8f3a01ac94
곽민규 <kwmingyu@naver.com>
2025-03-21 15:44:01 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.09 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -0,0 +1,10 @@
+function solution(my_string, n) {
+
+    var answer = "";
+    my_string.split("").forEach((e) => {
+        for(let i=0; i<n; i++){
+            answer += e;
+        }
+    });
+    return answer;
+}
