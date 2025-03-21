59f0b2808f2025076b265e7bae6ee2873e6f0803
곽민규 <kwmingyu@naver.com>
2025-03-21 13:18:23 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,11 @@
+function solution(price) {
+    if(price >= 500000){
+        price = price - (price*0.2);
+    } else if (price >= 300000) {
+        price = price - (price*0.1);
+    } else if (price >= 100000){
+        price = price - (price*0.05);
+    }
+    price = Math.floor(price);
+    return price;
+}
