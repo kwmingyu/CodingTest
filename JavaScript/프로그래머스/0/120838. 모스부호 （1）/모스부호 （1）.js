1b367e63ea054b6796a3faa1a54d10ebf4e6992b
곽민규 <kwmingyu@naver.com>
2025-03-27 09:44:44 +0900

    [level 0] Title: 모스부호 (1), Time: 0.25 ms, Memory: 33.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.js
+++ JavaScript/프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -1,7 +1,6 @@
 function solution(letter) {
     let morese = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."];
     let str = "abcdefghijklmnopqrstuvwxyz".split("");
-    let answer ="";
     
     let input = letter.split(" ").map((v) => morese.indexOf(v));
     return input.map((v) => str[v]).join("");
