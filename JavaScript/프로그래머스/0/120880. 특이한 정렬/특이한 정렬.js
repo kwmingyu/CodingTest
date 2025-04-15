94940e15117635070e441b479c1336ea73ced5f6
곽민규 <kwmingyu@naver.com>
2025-04-15 14:05:08 +0900

    [level 0] Title: 특이한 정렬, Time: 0.67 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/120880. 특이한 정렬/특이한 정렬.js
@@ -0,0 +1,8 @@
+function solution(numlist, n) {
+    return numlist.map(x => [x, Math.abs(x - n)])
+       .sort((a, b) => {
+           if (a[1] === b[1]) return b[0] - a[0]; // 거리 같으면 더 큰 수 우선
+           return a[1] - b[1]; // 거리 기준 정렬
+       })
+       .map(x => x[0]);
+}
